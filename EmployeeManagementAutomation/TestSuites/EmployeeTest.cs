5e3adae19b4e4de84e60dff5a501c56fa7869dc5
balaji-githubstore <balajidinakaran1@gmail.com>
2025-07-18 15:45:08 +0530

    Added Employee Test

--- EmployeeManagementAutomation/TestSuites/EmployeeTest.cs
+++ EmployeeManagementAutomation/TestSuites/EmployeeTest.cs
@@ -20,15 +20,23 @@ namespace EmployeeManagementAutomation.TestSuites
             driver.FindElement(By.Name("password")).SendKeys("admin123");
             driver.FindElement(By.XPath("//button[normalize-space()='Login']")).Click();
 
-            //Click on PIM menu
-            //Click on Add Employee
-            //Enter firstname as john
-            //middle name as w
-            //lastname as wick 
-            //save 
+            driver.FindElement(By.XPath("//span[text()='PIM']")).Click();
+            driver.FindElement(By.LinkText("Add Employee")).Click();
+            driver.FindElement(By.Name("firstName")).SendKeys("john");
+            driver.FindElement(By.Name("middleName")).SendKeys("w");
+            driver.FindElement(By.Name("lastName")).SendKeys("jack");
+            driver.FindElement(By.XPath("//button[normalize-space()='Save']")).Click();
 
             //Assert the profile name - john wick
+            string actualProfileName= driver.FindElement(By.XPath("//h6[contains(normalize-space(),'jack')]")).Text;
             //Assert the firstname textbox contains - john
+            string actualFirstName=driver.FindElement(By.Name("firstName")).GetAttribute("value");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualProfileName, Is.EqualTo("john jack"));
+                Assert.That(actualFirstName, Is.EqualTo("john"));
+            });
         }
     }
 }
